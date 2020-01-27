abb4c0c66a295dafa36aae7fb83b224dc0bd2c60
Jenesius <lokargenia@gmail.com>
2020-01-27 15:08:30 +0300

    Add routerWorker

--- src/plugin/index.js
+++ src/plugin/index.js
@@ -1,96 +1,38 @@
 import RouterView from './RouterView';
 import RouterLink from "./RouterLink";
 
+import RouterWorker from './routeWorker';
+
 import { listen, push, back } from "./history";
-
-import finder from "./finder";
-
-
-function update(routes){
-
-    let _arr = [];
-
-    function goTo(elem, path = ''){
-
-        let modifyPath = path + elem.path;
-
-        _arr.push({
-            path: modifyPath,
-            components: elem.components,
-        });
-
-        if (elem.hasOwnProperty('redirect')){
-            _arr[_arr.length -1].redirect = elem.redirect;
-        }
-
-
-        if (elem.children !== undefined){
-            elem.children.forEach(elem => {
-                goTo(elem, modifyPath);
-            })
-        }
-    }
-    routes.forEach(elem => {
-        goTo(elem);
-    });
-
-    return _arr;
-}
-function checkRedirect(routes, totalURL){
-
-
-
-    let currentElem = routes.find(elem =>   elem.path === totalURL);
-
-    if (currentElem.hasOwnProperty('redirect')){
-        return currentElem.redirect;
-    }
-    return false;
-}
 
 export default {
     install(Vue, options) {
 
-
-
-
         //Если установлен мод RemoveHash при первом запуске #hash будет удалён
         if (options.basic){
-            if (options.basic.removeHash) {
-                history.replaceState(null, null, ' ');
-            }
+            if (options.basic.removeHash)
+                RouterWorker.replaceState(' ');
         }
 
 
+        Vue.prototype.$routes = RouterWorker.adaptationRoutes(options.routes);
 
 
-        //Modify Router Array
-        Vue.prototype.$routes = update(options.routes);
 
-        let _tmp = checkRedirect(Vue.prototype.$routes, window.location.pathname + window.location.hash);
-
-        if (_tmp ){
-            // eslint-disable-next-line no-console
-            history.replaceState(null, null, _tmp);
-        }
+        let _tmp = RouterWorker.checkRedirect(Vue.prototype.$routes, window.location.pathname + window.location.hash);
+        RouterWorker.replaceState(_tmp);
 
 
+        /*Добавляем дефолтные компоненты для роутинга*/
         Vue.component("RouterView", RouterView);
         Vue.component("RouterLink", RouterLink);
 
 
-
-
-
         Vue.prototype.$pushRoute = (routePath) => {
 
-            let test = checkRedirect(Vue.prototype.$routes, routePath);
+            let redirectUrl = RouterWorker.checkRedirect(Vue.prototype.$routes, routePath);
 
-            if (test ){
-                routePath = test;
-            }
-
-            let _tmp = finder(Vue.prototype.$routes).findByPath(routePath);
+            let _tmp = RouterWorker.findRouteByPath(Vue.prototype.$routes, (redirectUrl || routePath));
 
             push(_tmp);
 
@@ -99,11 +41,11 @@ export default {
         //Если указать проверку, то переход назад состоится лишь тогда,
         // когда текущий путь равен полученному
         Vue.prototype.$backRoute = (_str ) => {
+
             if (_str === undefined) {
                 back();
                 return;
             }
-
             switch (_str[0]) {
                     case '#':
                         if (window.location.hash === _str) back();
@@ -112,7 +54,6 @@ export default {
                         if (window.location.pathname === _str) back();
                         break;
                 }
-
         };
 
         if (options.hook) {
